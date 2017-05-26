4b18fbb54b5f85e9e012caf6a7af515e82ca7e79
Alex <johnalexmarich@gmail.com>
2017-05-26 13:12:57 -0400

    Fixed typo 'tihs' to 'this'

--- CodingChallenges/CC_71_minesweeper/cell.js
+++ CodingChallenges/CC_71_minesweeper/cell.js
@@ -37,7 +37,7 @@ Cell.prototype.countBees = function() {
   }
   var total = 0;
   for (var xoff = -1; xoff <= 1; xoff++) {
-    var i = tihs.i + xoff;
+    var i = this.i + xoff;
     if (i < 0 || i >= cols) continue;
     
     for (var yoff = -1; yoff <= 1; yoff++) {
